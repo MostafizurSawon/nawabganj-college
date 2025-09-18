5c29b72b9603811baba91ec5fe964443798fcb87
Mostafizur Sawon <mostafizur1102@gmail.com>
2025-09-18 16:34:07 +0600

    student dashboard updated

--- apps/sample/urls.py
+++ apps/sample/urls.py
@@ -5,16 +5,18 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    # path("dashboard/", login_required(views.dashboard, name="index"),
-    # path("dashboard/", views.dashboard, name="index"),
     path("dashboard/", login_required(SampleView.as_view()), name="index"),
-    # path("dashboard/", login_required(SampleView.as_view(template_name="index.html")), name="index"),
 
     path("page_2/", SampleView.as_view(template_name="page_2.html"), name="page-2"),
     path(
-        "dashboard/my-admission-form/",
+        "dashboard/my-hsc-admission-form/",
         login_required(SampleView.as_view(extra_context={"mode": "list"})),
         name="my_form",
+    ),
+    path(
+        "dashboard/my-degree-admission-form/",
+        login_required(SampleView.as_view(extra_context={"mode": "degree"})),
+        name="my_degree_form",
     ),
     path(
         "dashboard/my-payment/",
